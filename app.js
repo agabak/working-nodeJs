cd8d96a956e4f6e1b41cc95a2ad452e982ab70de
Agaba Kabunga <Agaba Kabunga>
2017-02-05 17:24:49 -0600

    Add files

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-'use strict'
-var express = require('express');
-var app = express();
-var port = 5000
-
-app.use(express.static('public'));
-app.use(express.static('src/views'));
-
-app.get('/', function (req, res) {
-    res.send('Hello there');
-});
-app.get('/books', function (req, res) {
-    res.send('I  have books  here');
-});
-app.listen(port, function (err) {
-    console.log('runnung on port ' + port);
-});
+(function () {
+    'use strict'
+    var express = require('express');
+    var app = express();
+    var port = 5000
+    app.use(express.static('public'));
+    app.use(express.static('src/views'));
+    app.get('/', function (req, res) {
+        res.send('Hello there');
+    });
+    app.get('/books', function (req, res) {
+        res.send('I  have books  here');
+    });
+    app.listen(port, function (err) {
+        console.log('runnung on port ' + port);
+    });
+})();
